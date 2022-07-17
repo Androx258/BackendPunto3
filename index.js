dbbc484e62a350af7ac0d6b8faf8b6c90b495918
Androx258 <m.andresm94@gmail.com>
2022-07-17 13:11:58 -0500

    Borrar bd

--- index.js
+++ index.js
@@ -15,14 +15,3 @@ app.use(require('./server/routes/index'));
 app.listen(process.env.PORT, () => {
     console.log(`Conectado al puerto ${process.env.PORT}`);
 });
-//Conexion local
-//mongodb://localhost:27017/PPI
-
-mongoose.connect('mongodb+srv://admin:admin@cluster0-gzppv.mongodb.net/FINANZAS', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}, (err, res) => {
-    if (err) throw err;
-    console.log('Conectado a la base de datos');
-});
